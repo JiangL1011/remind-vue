bac11f5aa9e2cbb7aeffd42b27e7ee586b92a035
蒋领 <ling.jiang.1011@gmail.com>
2019-06-14 17:44:04 +0800

    编写calendar模块

--- src/renderer/components/calendar.vue
+++ src/renderer/components/calendar.vue
@@ -1,18 +1,22 @@
 <template>
     <div class="calendar calendar-outline">
+        <div class="calendar-realtime">
+            <div class="calendar-realtime-clock">{{ realtime }}</div>
+            <div class="calendar-realtime-date">{{ realDate }}</div>
+        </div>
         <div class="calendar-head">
             <div class="calendar-head-month-year">
-                <div class="calendar-head-month-year-angle">
+                <div class="calendar-head-month-year-angle" @click="changeYear(-1)">
                     <i class="fa fa-angle-double-left"></i>
                 </div>
-                <div class="calendar-head-month-year-angle">
+                <div class="calendar-head-month-year-angle" @click="changeMonth(-1)">
                     <i class="fa fa-angle-left"></i>
                 </div>
-                <div>{{ year+'年 '+month+'月' }}</div>
-                <div class="calendar-head-month-year-angle">
+                <div class="calendar-head-month-year-date">{{ year+'年 '+month+'月' }}</div>
+                <div class="calendar-head-month-year-angle" @click="changeMonth(1)">
                     <i class="fa fa-angle-right"></i>
                 </div>
-                <div class="calendar-head-month-year-angle">
+                <div class="calendar-head-month-year-angle" @click="changeYear(1)">
                     <i class="fa fa-angle-double-right"></i>
                 </div>
             </div>
@@ -21,8 +25,8 @@
             </div>
         </div>
         <div class="calendar-body">
-            <div class="calendar-body-day" v-for="day in days">
-                <span>{{ day }}</span>
+            <div :class="'calendar-body-day '+ day.isToday" @click="selectDay" v-for="day in days">
+                <span v-bind:class="day.dayType">{{ day.dayOfMonth }}</span>
             </div>
         </div>
     </div>
@@ -31,77 +35,164 @@
 <script>
   import moment from 'moment'
 
+  moment.locale('zh-cn')
+
   export default {
     name: 'calendar',
     data: function () {
       return {
-        year: '',
-        month: '',
         week: ['日', '一', '二', '三', '四', '五', '六'],
-        days: []
+        // days中的元素是：{dayOfMonth:1,dayType:'curr'}
+        days: [],
+        realtime: moment().format('hh:mm:ss a'),
+        realDate: moment().format('YYYY-MM-DD dddd'),
+        selectedDate: moment().format('YYYYMMDD')
       }
     },
     created: function () {
       this.initCalendar()
+      this.initClock()
     },
     methods: {
-      initCalendar () {
-        this.year = moment().format('YYYY')
-        this.month = moment().format('MM')
+      initCalendar: function (year, month) {
+        clearSelectedDay()
+        this.year = year || moment().format('YYYY')
+        this.month = month || moment().format('MM')
         const firstDayOfMonth = moment(this.year + this.month + '01', 'YYYYMMDD')
         let currLoopDay = firstDayOfMonth.add(firstDayOfMonth.day() * -1, 'd')
+        // 这三个值是calendar-body-day中的span的类，分别表示上月日期，当月日期，下月日期
+        const dayType = ['pre', 'curr', 'next']
+        let dayTypeIndex = 0
+        const today = moment().format('YYYYMMDD')
         for (let i = 0; i < 42; i++) {
-          this.days[i] = currLoopDay.get('date')
+          const currLoopDate = currLoopDay.format('YYYYMMDD')
+          const dayOfMonth = currLoopDay.get('date')
+          if (dayOfMonth === 1) {
+            dayTypeIndex++
+          }
+          this.days[i] = {
+            dayOfMonth,
+            dayType: dayType[dayTypeIndex],
+            isToday: today === currLoopDate ? 'today' : ''
+          }
           currLoopDay = currLoopDay.add(1, 'd')
         }
+      },
+      changeMonth: function (direct) {
+        const newMonth = moment(this.year + this.month, 'YYYYMM').add(1 * direct, 'M')
+        const year = newMonth.format('YYYY')
+        const month = newMonth.format('MM')
+        this.initCalendar(year, month)
+      },
+      changeYear: function (direct) {
+        const newYear = moment(this.year + this.month, 'YYYYMM').add(1 * direct, 'y')
+        const year = newYear.format('YYYY')
+        const month = newYear.format('MM')
+        this.initCalendar(year, month)
+      },
+      initClock: function () {
+        const that = this
+        setInterval(function () {
+          that.realtime = moment().format('hh:mm:ss a')
+        }, 1000)
+      },
+      selectDay: function (event) {
+        const el = event.currentTarget
+        clearSelectedDay()
+        el.classList.add('selected-day')
       }
     }
   }
-  // 00B8EC
-  // CEF1FB
+
+  const clearSelectedDay = function () {
+    const oldSelected = document.getElementsByClassName('selected-day')
+    if (oldSelected[0]) {
+      oldSelected[0].classList.remove('selected-day')
+    }
+  }
 </script>
 
 <style scoped>
-    /*@import "../assets/font-awesome-4.7.0/css/font-awesome.min.css";*/
     .calendar-outline {
-        height: 305px;
         width: 300px;
         border: 1px solid #CEF1FB;
-        padding-top: 2px;
-        padding-bottom: 2px;
     }
 
-    .calendar-head-week li {
-        list-style: none;
-        float: left;
-        margin-left: 0;
-        display: block;
+    .calendar-realtime {
+        height: 65px;
+        border-bottom: 1px solid #CEF1FB;
+        padding: 3px 14px 3px 14px;
     }
 
-    .calendar-head-week {
-        padding: 0;
+    .calendar-realtime-clock {
+        font-size: x-large;
+        font-weight: bolder;
+    }
+
+    .calendar-realtime-date {
+        font-size: small;
+    }
+
+    .calendar-head, .calendar-realtime {
+        background: #3361ff;
+        color: white;
     }
 
     .calendar-body, .calendar-head-week, .calendar-head-month-year {
         display: flex;
         flex-wrap: wrap;
         justify-content: space-around;
+        align-content: space-around;
+    }
+
+    .calendar-body {
+        margin-top: 2px;
+        height: 252px;
     }
 
     .calendar-body-day, .calendar-head-week div {
-        height: 38px;
+        height: 40px;
         width: 40px;
-        margin: 1px;
         text-align: center;
-        line-height: 38px;
+        line-height: 36px;
+        cursor: default;
     }
 
-    .calendar-head-month-year-angle{
+    .calendar-body-day {
+        border: 2px solid white;
+        border-radius: 20px;
+    }
+
+    .calendar-body-day:hover {
+        background: #eee6ee;
+    }
+
+    .calendar-head-month-year-angle {
         width: 28px;
         text-align: center;
         cursor: pointer;
     }
-    .calendar-head-month-year-angle:hover{
-        color: #3361ff;
+
+    .calendar-head-month-year-date {
+        cursor: default;
     }
+
+    .calendar-head-month-year-angle:hover {
+        color: #fffb01;
+    }
+
+    .pre, .next {
+        color: #adadad;
+    }
+
+    .today {
+        border-color: #3361ff;
+        color: white;
+        background: #3361ff;
+    }
+
+    .selected-day {
+        border-color: #3361ff;
+    }
+
 </style>
